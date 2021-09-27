70c0dbee252400cccc0695b4a4d38099bf6c9d49
Artowod <myjavaname@gmail.com>
2021-09-28 00:12:03 +0300

    ht-7 - done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryArea = document.querySelector('.gallery');
+galleryItems.forEach(({ preview, original, description }, index) => {
+  const galleryItem = `
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+  galleryArea.insertAdjacentHTML('beforeend', galleryItem);
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightbox.on('error.simplelightbox', e => {
+  console.log(`what is wrong: ${e}`);
+  alert('IMAGE LOAD ERROR !!! Please check images location!');
+});
